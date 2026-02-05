19b7675831ebc7dbe0486428cac2907bba34e710
Marcelo Domínguez <dmmarcelo27@gmail.com>
2026-02-05 13:06:01 +0100

    Bootstrap ejercicio1

---
+++ src/1-parallelogram-area.R
@@ -0,0 +1,11 @@
+# ============================================================
+# File: 1-parallelogram-area.R
+#
+# Authors:
+#   - Marcelo Domínguez (@sa4dus)
+#   - Alejandro García Prada (@AlexGarciaPrada)
+#
+# Created: 2026-02-05
+# Last modified: 2026-02-05
+# License: MIT
+# ============================================================
