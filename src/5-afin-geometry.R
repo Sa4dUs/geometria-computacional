5e05af82ccee56fda7f4706412062916468a6cfc
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2026-04-02 18:26:27 +0200

    Remade with better grafication cnd order

--- src/5-afin-geometry.R
+++ src/5-afin-geometry.R
@@ -1,42 +1,66 @@
-
 library(scatterplot3d)
 
-translatePoint2D <-function(point,vector ){
-    #if translation vector has more dimension we could translate also
-    if (length(point) != length(vector)){
-        stop("The vector length must be equal to the point length")
-    }
-    # plot only works in 2D i think
-    result = point+vector
-    plot(point, result, main="Traslation of a 2D point")
-    points(point[1],point[2],col="blue",pch = 16)
-    points(result[1],result[2],col="red",pch = 16)
-    abline(h = 0, v = 0, col = "gray")
-    arrows(point[1],point[2],result[1], result[2])
-    return (result)
+translatePoint2D <- function(point, vector) {
+  if (length(point) != length(vector)) stop("El vector y el punto deben tener la misma longitud")
+  
+  result <- point + vector
+  
+  xlim <- range(c(point[1], result[1], 0)) + c(-1, 1)
+  ylim <- range(c(point[2], result[2], 0)) + c(-1, 1)
+  
+  plot(rbind(point, result), type="n", asp=1, xlim=xlim, ylim=ylim,
+       main="2D Translation", xlab="X", ylab="Y")
+  grid()
+  abline(h = 0, v = 0, col = "gray")
+  
+  points(point[1], point[2], col="blue", pch = 16)
+  points(result[1], result[2], col="red", pch = 16)
+  arrows(point[1], point[2], result[1], result[2], lwd=2)
+  
+  text(point[1], point[2], "P", pos=3, col="blue")
+  text(result[1], result[2], "P'", pos=3, col="red")
+  
+  return(result)
 }
 
+translatePoint3D <- function(point, vector) {
+  result <- point + vector
+  datos <- rbind(matrix(point, nrow=1), matrix(result, nrow=1))
+  
+  s3d <- scatterplot3d(datos, pch=16, main="3D Translation", angle=55)
+  s3d$points3d(point[1], point[2], point[3], col="blue", pch=16)
+  s3d$points3d(result[1], result[2], result[3], col="red", pch=16)
+  
+  p1 <- s3d$xyz.convert(point[1], point[2], point[3])
+  p2 <- s3d$xyz.convert(result[1], result[2], result[3])
+  arrows(p1$x, p1$y, p2$x, p2$y, col="black", lwd=2, length=0.1)
+  
+  return(result)
+}
 
 rotatePointIn2d <- function(point, center, angle) {
-  #Angle enters in degrees
   theta <- angle * (pi / 180)
   
-  #Rotation matrix 2x2
-  matrix <- matrix(c(cos(theta),  sin(theta), 
-                    -sin(theta), cos(theta)), nrow = 2)
-  res <- (point - center) %*% matrix + center
+  matrix_rot <- matrix(c(cos(theta), -sin(theta), 
+                         sin(theta),  cos(theta)), nrow = 2, byrow = TRUE)
+  
+  res <- matrix_rot %*% (point - center) + center
   result <- as.vector(res)
-  x_vals <- c(point[1], result[1], center[1])
-  y_vals <- c(point[2], result[2], center[2])
-  
-  plot(x_vals, y_vals,
-       main = "Rotation of a 2D point",
-       type = "n",
-       xlab = "X", ylab = "Y",
-       asp = 1)
-  
-  abline(h = 0, v = 0, col = "gray")
 
+  r <- sqrt(sum((point - center)^2)) * 0.5
+  start_angle <- atan2(point[2]-center[2], point[1]-center[1])
+  t_seq <- seq(0, theta, length.out = 100)
+  arc_x <- center[1] + r * cos(start_angle + t_seq)
+  arc_y <- center[2] + r * sin(start_angle + t_seq)
+  
+  all_x <- c(point[1], result[1], center[1], arc_x)
+  all_y <- c(point[2], result[2], center[2], arc_y)
+  
+  plot(all_x, all_y, type="n", asp=1, xlab="X", ylab="Y", 
+       xlim=range(all_x) + c(-0.5, 0.5), ylim=range(all_y) + c(-0.5, 0.5),
+       main=paste("2D Rotation (", angle, "°)", sep=""))
+  grid(); abline(h = 0, v = 0, col = "gray")
+  
   points(point[1], point[2], col = "blue", pch = 16)
   points(result[1], result[2], col = "green", pch = 16)
   points(center[1], center[2], col = "red", pch = 16)
@@ -44,210 +68,165 @@ rotatePointIn2d <- function(point, center, angle) {
   segments(center[1], center[2], point[1], point[2], lty = 2)
   segments(center[1], center[2], result[1], result[2], lty = 2)
   
-  arrows(point[1], point[2], result[1], result[2], lwd = 2)
-  
-  r <- sqrt(sum((point - center)^2)) * 0.5
-  
-  t_seq <- seq(0, theta, length.out = 100)
-  
-  arc_x <- center[1] + r * cos(t_seq + atan2(point[2]-center[2], point[1]-center[1]))
-  arc_y <- center[2] + r * sin(t_seq + atan2(point[2]-center[2], point[1]-center[1]))
-  
   lines(arc_x, arc_y, col = "purple", lwd = 2)
   
-  text(arc_x[length(arc_x)], arc_y[length(arc_y)],
-       paste0(angle, "°"), col = "purple", pos = 4)
+  mid_index <- floor(length(t_seq)/2)
+  r_offset <- 0.25 * r
+  text(center[1] + (r + r_offset) * cos(start_angle + t_seq[mid_index]),
+       center[2] + (r + r_offset) * sin(start_angle + t_seq[mid_index]),
+       paste0(angle, "°"), col = "purple", cex = 0.9)
   
   text(point[1], point[2], "P", pos = 3, col = "blue")
   text(result[1], result[2], "P'", pos = 3, col = "green")
   text(center[1], center[2], "C", pos = 3, col = "red")
-
-
-  
   
   return(result)
 }
-
 rotatePointIn3d <- function(point, center, angle, axis = "z") {
   theta <- angle * (pi / 180)
   s <- sin(theta)
   c <- cos(theta)
   
-  matrix <- switch(tolower(axis),
-    "x" = matrix(c(1, 0, 0,  0, c, s,  0, -s, c), nrow = 3),
-    "y" = matrix(c(c, 0, -s, 0, 1, 0,  s, 0, c), nrow = 3),
-    "z" = matrix(c(c, s, 0, -s, c, 0,  0, 0, 1), nrow = 3),
-    stop("Axis not valid. Use 'x', 'y' o 'z'.")
+  matrix_rot <- switch(tolower(axis),
+                       "x" = matrix(c(1, 0, 0,  0, c, s,  0, -s, c), nrow = 3),
+                       "y" = matrix(c(c, 0, -s, 0, 1, 0,  s, 0, c), nrow = 3),
+                       "z" = matrix(c(c, s, 0, -s, c, 0,  0, 0, 1), nrow = 3),
+                       stop("Eje no válido. Usa 'x', 'y' o 'z'.")
   )
   
-  res <- (point - center) %*% matrix + center
-  return(as.vector(res))
+  res <- (point - center) %*% matrix_rot + center
+  result <- as.vector(res)
+
+  datos <- rbind(matrix(point, nrow=1), matrix(result, nrow=1), matrix(center, nrow=1))
+  s3d <- scatterplot3d(datos, type="n", main=paste("3D Rotation (Axis", toupper(axis), ")"), angle=55)
+  
+  s3d$points3d(point[1], point[2], point[3], col="blue", pch=16)
+  s3d$points3d(result[1], result[2], result[3], col="green", pch=16)
+  s3d$points3d(center[1], center[2], center[3], col="red", pch=16)
+  
+  p_start <- s3d$xyz.convert(point[1], point[2], point[3])
+  p_end   <- s3d$xyz.convert(result[1], result[2], result[3])
+  p_cent  <- s3d$xyz.convert(center[1], center[2], center[3])
+  
+  segments(p_cent$x, p_cent$y, p_start$x, p_start$y, lty=2)
+  segments(p_cent$x, p_cent$y, p_end$x, p_end$y, lty=2)
+  
+  return(result)
 }
 
-#For entering the hiperplane the inputs are the coefficients
-simetricPointRn <- function(point, coefs) {
+
+simetricPoint2D <- function(point, coefs) {
+  normal <- coefs[1:2]
+  D <- coefs[3]
+  factor <- (sum(normal * point) + D) / sum(normal^2)
+  result <- as.vector(point - 2 * factor * normal)
   
-  n <- length(point)
+  xlim <- range(c(point[1], result[1], 0)) + c(-2, 2)
+  ylim <- range(c(point[2], result[2], 0)) + c(-2, 2)
   
-  if (length(coefs) != n + 1) {
-    stop("Los coeficientes deben tener longitud n + 1")
+  plot(rbind(point, result), type="n", asp=1, xlim=xlim, ylim=ylim,
+       main="2D Symmetry", xlab="X", ylab="Y")
+  grid(); abline(h=0, v=0, col="gray")
+  
+  if(coefs[2] != 0){
+    abline(a = -coefs[3]/coefs[2], b = -coefs[1]/coefs[2], lwd=2, col="darkgray")
+  } else {
+    abline(v = -coefs[3]/coefs[1], lwd=2, col="darkgray")
   }
   
-  normal <- coefs[1:n]
-  D <- coefs[n + 1]
+  points(point[1], point[2], col="blue", pch=16)
+  points(result[1], result[2], col="orange", pch=16)
+  segments(point[1], point[2], result[1], result[2], lty=2)
   
-  n_norm_sq <- sum(normal^2)
+  text(point[1], point[2], "P", pos=3, col="blue")
+  text(result[1], result[2], "P'", pos=3, col="orange")
   
-  factor <- (sum(normal * point) + D) / n_norm_sq
-  
-  simetric <- point - 2 * factor * normal
-  
-  return(as.vector(simetric))
+  return(result)
 }
 
-homothetyPoint <- function(point, center, k) {
-  n <- length(point)
+simetricPoint3D <- function(point, coefs) {
+  normal <- coefs[1:3]
+  D <- coefs[4]
+  factor <- (sum(normal * point) + D) / sum(normal^2)
+  result <- as.vector(point - 2 * factor * normal)
   
-  if (length(center) != n) {
-    stop("Point and center must have the same dimension")
-  }
+  datos <- rbind(matrix(point, nrow = 1), matrix(result, nrow = 1))
+  s3d <- scatterplot3d(datos, pch=16, main="3D Symmetry", angle=55)
+  
+  s3d$points3d(point[1], point[2], point[3], col="blue")
+  s3d$points3d(result[1], result[2], result[3], col="orange")
+  
+  p1 <- s3d$xyz.convert(point[1], point[2], point[3])
+  p2 <- s3d$xyz.convert(result[1], result[2], result[3])
+  segments(p1$x, p1$y, p2$x, p2$y, lty=2)
+  
+  return(result)
+}
 
-  matrix_h <- diag(k, n, n)
-  
+
+homothetyPoint2D <- function(point, center, k) {
+  matrix_h <- diag(k, 2, 2)
   res <- (point - center) %*% matrix_h + center
+  result <- as.vector(res)
   
-  return(as.vector(res))
+  xlim <- range(c(point[1], result[1], center[1])) + c(-1, 1)
+  ylim <- range(c(point[2], result[2], center[2])) + c(-1, 1)
+  
+  plot(rbind(point, result, center), type="n", asp=1, xlim=xlim, ylim=ylim,
+       main=paste("2D Homothety (k =", k, ")"), xlab="X", ylab="Y")
+  grid(); abline(h=0, v=0, col="gray")
+  
+  points(point[1], point[2], col="blue", pch=16)
+  points(result[1], result[2], col="purple", pch=16)
+  points(center[1], center[2], col="red", pch=4)
+  
+  segments(center[1], center[2], result[1], result[2], lty=2)
+  
+  text(point[1], point[2], "P", pos=3, col="blue")
+  text(result[1], result[2], "P'", pos=3, col="purple")
+  text(center[1], center[2], "C", pos=3, col="red")
+  
+  return(result)
 }
-P2 <- matrix(c(1, 2, 
-               3, 4, 
-               0, 0), ncol = 2, byrow = TRUE)
 
-v_trans <- c(-1, 4)
-c_rot <- c(0, 0)
-ang_rot <- 45
-coefs_sim <- c(2, -1, -1)
-c_hom <- c(2, -2)
-k_hom <- 5
-
-res_trans_2d <- t(apply(P2, 1, translatePoint2D, vector = v_trans))
-res_rot_2d   <- t(apply(P2, 1, rotatePointIn2d, center = c_rot, angle = ang_rot))
-res_sim_2d   <- t(apply(P2, 1, simetricPoint2d, coefs = coefs_sim))
-res_hom_2d   <- t(apply(P2, 1, homothetyPoint, center = c_hom, k = k_hom))
-
-
-
-par(mfrow=c(1,1)) 
-
-# --- 2D Translation ---
-plot(rbind(P2, res_trans_2d), type="n", main="2D Translation", xlab="X", ylab="Y", asp=1)
-grid()
-abline(h = 0, v = 0, col = "gray")
-points(P2, col="blue", pch=16, cex=1.5)
-points(res_trans_2d, col="red", pch=16, cex=1.5)
-arrows(P2[,1], P2[,2], res_trans_2d[,1], res_trans_2d[,2], length=0.1, col="black")
-text(P2[,1], P2[,2], labels=paste0("P", 1:3), pos=1, col="blue", cex=0.8)
-text(res_trans_2d[,1], res_trans_2d[,2], labels=paste0("P'", 1:3), pos=3, col="red", cex=0.8)
-
-# --- 2D Rotation ---
-plot(rbind(P2, res_rot_2d, c_rot), type="n", main="2D Rotation", xlab="X", ylab="Y", asp=1)
-grid()
-abline(h = 0, v = 0, col = "gray")
-points(P2, col="blue", pch=16, cex=1.5)
-points(res_rot_2d, col="green", pch=16, cex=1.5)
-points(c_rot[1], c_rot[2], col="red", pch=4, cex=2, lwd=2)
-segments(c_rot[1], c_rot[2], P2[,1], P2[,2], col="blue", lty=2)
-segments(c_rot[1], c_rot[2], res_rot_2d[,1], res_rot_2d[,2], col="green", lty=2)
-arrows(P2[,1], P2[,2], res_rot_2d[,1], res_rot_2d[,2], length=0.1, col="black")
-text(P2[,1], P2[,2], labels=paste0("P", 1:3), pos=1, col="blue", cex=0.8)
-text(res_rot_2d[,1], res_rot_2d[,2], labels=paste0("P'", 1:3), pos=3, col="green", cex=0.8)
-
-# --- 2D Simetry---
-plot(rbind(P2, res_sim_2d), type="n", main="2D Simetry", xlab="X", ylab="Y", asp=1)
-grid()
-abline(h = 0, v = 0, col = "gray")
-
-intercept_sim <- -coefs_sim[3]/coefs_sim[2]
-slope_sim <- -coefs_sim[1]/coefs_sim[2]
-abline(a=intercept_sim, b=slope_sim, col="black", lwd=2) 
-points(P2, col="blue", pch=16, cex=1.5)
-points(res_sim_2d, col="orange", pch=16, cex=1.5)
-segments(P2[,1], P2[,2], res_sim_2d[,1], res_sim_2d[,2], lty=2, col="gray")
-text(P2[,1], P2[,2], labels=paste0("P", 1:3), pos=2, col="blue", cex=0.8)
-text(res_sim_2d[,1], res_sim_2d[,2], labels=paste0("P'", 1:3), pos=4, col="orange", cex=0.8)
-
-# --- 2D Homotethy ---
-plot(rbind(P2, res_hom_2d, c_hom), type="n", main="2D Homotethy", xlab="X", ylab="Y", asp=1)
-grid()
-abline(h = 0, v = 0, col = "gray")
-points(P2, col="blue", pch=16, cex=1.5)
-points(res_hom_2d, col="purple", pch=16, cex=1.5)
-points(c_hom[1], c_hom[2], col="red", pch=4, cex=2, lwd=2)
-segments(c_hom[1], c_hom[2], res_hom_2d[,1], res_hom_2d[,2], lty=2, col="gray")
-text(P2[,1], P2[,2], labels=paste0("P", 1:3), pos=4, col="blue", cex=0.8)
-text(res_hom_2d[,1], res_hom_2d[,2], labels=paste0("P'", 1:3), pos=2, col="purple", cex=0.8)
-
-
-P3 <- matrix(c(1, 2, 1, 
-               3, 4, 2, 
-               0, 0, 5), ncol = 3, byrow = TRUE)
-
-v_trans_3d <- c(2, -1, 3)
-c_rot_3d   <- c(0, 0, 0)
-ang_rot_3d <- 60
-coefs_plano_sim <- c(0, 0, 1, 0)
-c_hom_3d   <- c(1, 1, 1)
-k_hom_3d   <- 2
-
-
-res_trans_3d <- t(apply(P3, 1, function(p) p + v_trans_3d))
-res_rot_3d <- t(apply(P3, 1, rotatePointIn3d, center = c_rot_3d, angle = ang_rot_3d, axis = "z"))
-res_sim_3d <- t(apply(P3, 1, simetricPoint3d, coefs = coefs_plano_sim))
-res_hom_3d <- t(apply(P3, 1, homothetyPoint, center = c_hom_3d, k = k_hom_3d))
-
-
-# --- 3D Translation ---
-s3d <- scatterplot3d(rbind(P3, res_trans_3d), pch = 16, color = "lightgray",
-                     main = "3D Translation", xlab="X", ylab="Y", zlab="Z")
-s3d$points3d(P3, col = "blue", pch = 16, cex = 1.5)
-s3d$points3d(res_trans_3d, col = "red", pch = 16, cex = 1.5)
-p1 <- s3d$xyz.convert(P3)
-p2 <- s3d$xyz.convert(res_trans_3d)
-arrows(p1$x, p1$y, p2$x, p2$y, length = 0.1, col = "black")
-legend("topleft", legend = c("Original", "Translated"), col = c("blue", "red"), pch = 16)
-
-# --- 3D Rotation ---
-s3d <- scatterplot3d(rbind(P3, res_rot_3d), pch = 16, color = "lightgray",
-                     main = "3D Rotation", asp=1)
-s3d$points3d(P3, col = "blue", pch = 16, cex = 1.5)
-s3d$points3d(res_rot_3d, col = "green", pch = 16, cex = 1.5)
-p_centro <- s3d$xyz.convert(c_rot_3d[1], c_rot_3d[2], c_rot_3d[3])
-points(p_centro$x, p_centro$y, col="red", pch=4, lwd=2)
-legend("topleft", legend = c("Original", "Rotated"), col = c("blue", "green"), pch = 16)
-
-# --- 3D Simetry ---
-s3d <- scatterplot3d(rbind(P3, res_sim_3d), pch = 16, color = "white",
-                     main = "3D Simetry")
-
-s3d$points3d(P3, col = "blue", pch = 16, cex = 1.5)
-s3d$points3d(res_sim_3d, col = "orange", pch = 16, cex = 1.5)
-
-for(i in 1:nrow(P3)){
-  orig <- s3d$xyz.convert(P3[i,1], P3[i,2], P3[i,3])
-  refl <- s3d$xyz.convert(res_sim_3d[i,1], res_sim_3d[i,2], res_sim_3d[i,3])
-  segments(orig$x, orig$y, refl$x, refl$y, lty = 2, col = "gray")
+homothetyPoint3D <- function(point, center, k) {
+  matrix_h <- diag(k, 3, 3)
+  res <- (point - center) %*% matrix_h + center
+  result <- as.vector(res)
+  
+  datos <- rbind(matrix(point, nrow=1), matrix(result, nrow=1), matrix(center, nrow=1))
+  s3d <- scatterplot3d(datos, type="n", main=paste("3D Homothety (k =", k, ")"), angle=55)
+  
+  s3d$points3d(point[1], point[2], point[3], col="blue", pch=16)
+  s3d$points3d(result[1], result[2], result[3], col="purple", pch=16)
+  
+  cent <- s3d$xyz.convert(center[1], center[2], center[3])
+  points(cent$x, cent$y, col="red", pch=4, cex=1.5)
+  
+  p1 <- s3d$xyz.convert(point[1], point[2], point[3])
+  p2 <- s3d$xyz.convert(result[1], result[2], result[3])
+  
+  segments(cent$x, cent$y, p2$x, p2$y, lty=2)
+  
+  return(result)
 }
-legend("topleft", legend = c("Original", "Reflected"), col = c("blue", "orange"), pch = 16)
 
-# --- 3D Homotethy ---
-s3d <- scatterplot3d(rbind(P3, res_hom_3d), pch = 16, color = "white",
-                     main = "3D Homotethy")
-s3d$points3d(P3, col = "blue", pch = 16, cex = 1.5)
-s3d$points3d(res_hom_3d, col = "purple", pch = 16, cex = 1.5)
-p_centro_h <- s3d$xyz.convert(c_hom_3d[1], c_hom_3d[2], c_hom_3d[3])
-points(p_centro_h$x, p_centro_h$y, col="red", pch=4, lwd=2)
-for(i in 1:nrow(P3)){
-  orig <- s3d$xyz.convert(c_hom_3d[1], c_hom_3d[2], c_hom_3d[3])
-  dest <- s3d$xyz.convert(res_hom_3d[i,1], res_hom_3d[i,2], res_hom_3d[i,3])
-  segments(orig$x, orig$y, dest$x, dest$y, lty = 3, col = "gray")
-}
-legend("topleft", legend = c("Original", "Scaled"), col = c("blue", "purple"), pch = 16)
+
+
+par(mfrow=c(2, 2))
+
+P2 <- c(2, -1)
+res_trans_2d <- translatePoint2D(P2, c(-1, 4))
+res_rot_2d   <- rotatePointIn2d(P2, center = c(0, 0), angle = 45)
+res_sim_2d   <- simetricPoint2D(P2, coefs = c(2, -1, -1))
+res_hom_2d   <- homothetyPoint2D(P2, center = c(2, -2), k = 5)
+
+par(mfrow=c(2, 2))
+
+P3 <- c(2, -1, 3)
+res_trans_3d <- translatePoint3D(P3, vector = c(2, -1, 3))
+res_rot_3d   <- rotatePointIn3d(P3, center = c(0, 0, 0), angle = 60, axis = "z")
+res_sim_3d   <- simetricPoint3D(P3, coefs = c(0, 0, 1, 0))
+res_hom_3d   <- homothetyPoint3D(P3, center = c(1, 1, 1), k = 2)
+
+par(mfrow=c(1,1))
